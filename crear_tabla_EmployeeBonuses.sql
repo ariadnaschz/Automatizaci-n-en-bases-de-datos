efbff2cde3320694a6bfb902527a139e23056052
Ariadna Sánchez <151090907+ariadnaschz@users.noreply.github.com>
2025-06-18 15:37:43 -0500

    Update crear_tabla_EmployeeBonuses.sql

--- crear_tabla_EmployeeBonuses.sql
+++ crear_tabla_EmployeeBonuses.sql
@@ -1,13 +1,56 @@
-IF TYPE_ID(N'OrderDetailType') IS NULL
-BEGIN
-    CREATE TYPE OrderDetailType AS TABLE (
-        ProductID INT NOT NULL,
-        UnitPrice MONEY NOT NULL,
-        Quantity SMALLINT NOT NULL,
-        Discount REAL NOT NULL CHECK (Discount >= 0 AND Discount <= 1)
-    );
-    PRINT 'Tipo de tabla OrderDetailType creado exitosamente.';
-END
-ELSE
-    PRINT 'Tipo de tabla OrderDetailType ya existe.';
-GO
+-- ==========================================================================================
+
+-- Mejora el rendimiento y reduce el ruido en la salida de ejecución.
+SET NOCOUNT ON;
+
+-- Inicia un bloque TRY para el manejo de errores durante la creación de la tabla.
+BEGIN TRY
+ 
+    IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[EmployeeBonuses]') AND type in (N'U'))
+    BEGIN
+ 
+        PRINT 'Creando la tabla [dbo].[EmployeeBonuses]...';
+
+
+        CREATE TABLE [dbo].[EmployeeBonuses](
+           
+            [BonusID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,    
+            [EmployeeID] [int] NOT NULL,
+            [BonusMonth] [int] NOT NULL,
+            [BonusYear] [int] NOT NULL,
+            [NumberOfOrdersManaged] [int] NOT NULL,
+            [BonusAmount] [money] NOT NULL,
+            [CalculationDate] [datetime] NULL DEFAULT (GETDATE()),
+            CONSTRAINT FK_EmployeeBonuses_Employees FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Employees]([EmployeeID]),
+
+            CONSTRAINT CK_BonusMonth CHECK ([BonusMonth] BETWEEN 1 AND 12),
+
+            CONSTRAINT UK_EmployeeBonus_MonthYear UNIQUE ([EmployeeID], [BonusMonth], [BonusYear])
+        );
+
+        PRINT 'Tabla [dbo].[EmployeeBonuses] creada exitosamente.';
+    END
+    ELSE
+    BEGIN
+        -- Mensaje informativo si la tabla ya existía y no se realizó ninguna acción de creación.
+        PRINT 'La tabla [dbo].[EmployeeBonuses] ya existe. No se requieren acciones.';
+    END
+END TRY
+-- Bloque CATCH para capturar cualquier error que ocurra durante la ejecución del bloque TRY.
+BEGIN CATCH
+
+    PRINT 'Ocurrió un error al intentar crear la tabla [dbo].[EmployeeBonuses].';
+    PRINT 'Error Número: ' + CAST(ERROR_NUMBER() AS VARCHAR(10));
+    PRINT 'Mensaje de Error: ' + ERROR_MESSAGE();
+    PRINT 'Severidad del Error: ' + CAST(ERROR_SEVERITY() AS VARCHAR(10));
+    PRINT 'Estado del Error: ' + CAST(ERROR_STATE() AS VARCHAR(10));
+    PRINT 'Línea del Error: ' + CAST(ERROR_LINE() AS VARCHAR(10));
+
+
+    IF @@TRANCOUNT > 0
+        ROLLBACK TRANSACTION;
+END CATCH
+
+-- Restaura el comportamiento predeterminado de devolver mensajes de recuento de filas.
+SET NOCOUNT OFF;
+GO -- Final del lote de Transact-SQL.
